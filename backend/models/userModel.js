895369b586cb31990f0249a501d5dd5c7fb981a1
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-07-07 15:32:50 +0530

    auth routes done

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"name is required"]
+    },
+    email:{
+        type:String,
+        required:[true,"email is required"],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"],
+        minlength:[6,"password must be at least 6 characters long"]
+    }
+},{
+    timestamps:true
+})
+
+const User=mongoose.model('User',userSchema);
+module.exports=User;
